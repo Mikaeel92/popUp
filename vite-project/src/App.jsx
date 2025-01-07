f24fd5953f9f80a9b0398114ecf3a8b93c30cfad
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-07 09:04:45 +0330

    functionality & done!
    complete second component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
     setShowPopUp(!showPopUp)
   }
 
+  const handleClose = () => {
+    setShowPopUp(false)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center bg-gray-200 w-screen h-screen gap-4'>
       <button 
@@ -16,7 +20,13 @@ const App = () => {
       className="rounded-md p-2 bg-blue-800 text-white hover:bg-blue-900">Open Modal Popup!</button>
       {
         showPopUp ?
-          <PopUp />
+          <PopUp
+          id={7}
+          onClose={handleClose}
+          header={<h1>This Is Custom Header!</h1>} 
+          body={<h2>This Is Custom Body!</h2>} 
+          footer={<p>This Is Custom Footer!</p>}
+          />
         : null
       }
     </div>
--- vite-project/src/PopUp.jsx
+++ vite-project/src/PopUp.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const PopUp = () => {
+const PopUp = ({id, onClose, header, body, footer }) => {
   return (
-    <div>PopUp</div>
+    <div>
+        <div id={id || 'anything'} className='flex flex-col items-center justify-center w-[600px] h-[400px] bg-blue-200 rounded-md shadow-md'>
+        <span onClick={onClose} className='cursor-pointer font-bold text-2xl hover:text-3xl mb-2'>X</span>
+        <div className='bg-blue-300 w-80 h-20 rounded-t-md flex items-center justify-center'>
+            {header}
+        </div>
+        <div className='bg-blue-500 w-80 h-20 flex items-center justify-center'>
+            {body}
+        </div>
+        <div className='bg-blue-700 w-80 h-20 rounded-b-md flex items-center justify-center'>
+            {footer}
+        </div>
+        </div>
+    </div>
   )
 }
 
