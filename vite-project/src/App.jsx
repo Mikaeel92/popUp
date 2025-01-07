0bb72411e09e9e85c7281f90358733f22be9960b
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-07 08:58:18 +0330

    styling & state & second component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PopUp from './PopUp'
 
 const App = () => {
+
+  const [showPopUp, setShowPopUp] = useState(false)
+
+  const handlePopup = () => {
+    setShowPopUp(!showPopUp)
+  }
+
   return (
-    <div>App</div>
+    <div className='flex flex-col items-center justify-center bg-gray-200 w-screen h-screen gap-4'>
+      <button 
+      onClick={handlePopup}
+      className="rounded-md p-2 bg-blue-800 text-white hover:bg-blue-900">Open Modal Popup!</button>
+      {
+        showPopUp ?
+          <PopUp />
+        : null
+      }
+    </div>
   )
 }
 
